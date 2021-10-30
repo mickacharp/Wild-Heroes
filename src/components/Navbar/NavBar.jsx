93d220d766524f81cae9fcde4df7eb6497734890
jarod79 <arrijuria.eric@gmail.com>
2021-10-30 11:06:22 +0200

    version demo 1.0

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -7,11 +7,7 @@ const Navbar = () => {
     <header className="header">
       <div className="header-content">
         <a href="/" className="logo-content">
-          <img
-            src="https://i.ibb.co/jfcZ5Mr/Logo.png"
-            className="logo"
-            alt="Logo"
-          />
+          <img src="../Logo.png" className="logo" alt="Logo" />
         </a>
         <h1>WILD HEROES</h1>
         <div className="header-content-link">
