83544217c0d08758c81b5c809aa1e67ab9f83370
Jarod79 <89936533+Jarod79@users.noreply.github.com>
2021-11-09 15:47:16 +0100

    Modify filter search

--- src/components/Informations/Filter.jsx
+++ src/components/Informations/Filter.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+import img from '../../img/Logo.png';
 
 const Filter = ({ setFilter, filter, response }) => {
+  const [select, setselect] = useState(false);
+  console.log(select);
   return (
     <div className="boxes">
+      <img
+        className="logo-filter"
+        src={img}
+        alt={filter}
+        onMouseEnter={() => setselect(true)}
+        onMouseLeave={() => setselect(false)}
+      />
       <select
-        className="comics-filter"
+        className={select ? 'comics-filter' : 'hide-comics-filter'}
         onChange={(e) => setFilter(e.target.value)}
+        onMouseEnter={() => setselect(true)}
+        onMouseLeave={() => setselect(false)}
       >
         <option value=""> {filter} </option>
         {response.map((element) => (
@@ -15,6 +27,7 @@ const Filter = ({ setFilter, filter, response }) => {
           </option>
         ))}
       </select>
+      )
     </div>
   );
 };
