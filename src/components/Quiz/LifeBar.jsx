051c53e45b3f7a70f946fba961b79f5d6b660dc7
ValentinLagues <valentin.lagues@gmail.com>
2021-11-17 00:42:12 +0100

    img qui marchent pas

--- src/components/Quiz/LifeBar.jsx
+++ src/components/Quiz/LifeBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'react-step-progress-bar/styles.css';
 import './lifebar.css';
 import { ProgressBar, Step } from 'react-step-progress-bar';
+// import PropTypes from 'prop-types';
 
 const transitionStyles = {
   entering: { transform: 'scale(1.5)' },
@@ -15,123 +16,121 @@ const StepProgressBar = () => {
     <div className="progress-bar">
       <ProgressBar>
         <Step>
-          {({ accomplished, transitionState, index }) => (
+          {({ accomplished, transitionState }) => (
             <div
               style={transitionStyles[transitionState]}
               className={`customStep ${accomplished ? 'accomplished' : ''}`}
               percent={0}
-            >
-              {index}
-            </div>
+            />
           )}
         </Step>
         <Step>
-          {({ accomplished, transitionState, index }) => (
+          {({ accomplished, transitionState }) => (
             <div
               style={transitionStyles[transitionState]}
               className={`customStep ${accomplished ? 'accomplished' : ''}`}
               percent={10}
             >
-              {index}
+              <img src="../../img/Robin.png" alt="Robin" width="30" />
             </div>
           )}
         </Step>
         <Step>
-          {({ accomplished, transitionState, index }) => (
+          {({ accomplished, transitionState }) => (
             <div
               style={transitionStyles[transitionState]}
               className={`customStep ${accomplished ? 'accomplished' : ''}`}
               percent={20}
             >
-              {index}
+              <img src="../../img/Flash.png" alt="Flash" width="30" />
             </div>
           )}
         </Step>
         <Step>
-          {({ accomplished, transitionState, index }) => (
+          {({ accomplished, transitionState }) => (
             <div
               style={transitionStyles[transitionState]}
               className={`customStep ${accomplished ? 'accomplished' : ''}`}
               percent={30}
             >
-              {index}
+              <img src="../../img/Ironman.png" alt="Ironman" width="30" />
             </div>
           )}
         </Step>
         <Step>
-          {({ accomplished, transitionState, index }) => (
+          {({ accomplished, transitionState }) => (
             <div
               style={transitionStyles[transitionState]}
               className={`customStep ${accomplished ? 'accomplished' : ''}`}
               percent={40}
             >
-              {index}
+              <img src="../../img/CapAmerica.png" alt="America" width="30" />
             </div>
           )}
         </Step>
         <Step>
-          {({ accomplished, transitionState, index }) => (
+          {({ accomplished, transitionState }) => (
             <div
               style={transitionStyles[transitionState]}
               className={`customStep ${accomplished ? 'accomplished' : ''}`}
               percent={50}
             >
-              {index}
+              <img src="../../img/Spiderman-bar.png" alt="Spidey" width="30" />
             </div>
           )}
         </Step>
         <Step>
-          {({ accomplished, transitionState, index }) => (
+          {({ accomplished, transitionState }) => (
             <div
               style={transitionStyles[transitionState]}
               className={`customStep ${accomplished ? 'accomplished' : ''}`}
               percent={60}
             >
-              {index}
+              <img src="../../img/Batman.png" alt="Batman" width="30" />
             </div>
           )}
         </Step>
         <Step>
-          {({ accomplished, transitionState, index }) => (
+          {({ accomplished, transitionState }) => (
             <div
               style={transitionStyles[transitionState]}
               className={`customStep ${accomplished ? 'accomplished' : ''}`}
               percent={70}
             >
-              {index}
+              <img src="../../img/Wonderwoman.png" alt="WW" width="30" />
             </div>
           )}
         </Step>
         <Step>
-          {({ accomplished, transitionState, index }) => (
+          {({ accomplished, transitionState }) => (
             <div
               style={transitionStyles[transitionState]}
               className={`customStep ${accomplished ? 'accomplished' : ''}`}
               percent={80}
             >
-              {index}
+              <img src="../../img/Manhattan.png" alt="Manhattan" width="30" />
             </div>
           )}
         </Step>
         <Step>
-          {({ accomplished, transitionState, index }) => (
+          {({ accomplished, transitionState }) => (
             <div
               style={transitionStyles[transitionState]}
               className={`customStep ${accomplished ? 'accomplished' : ''}`}
               percent={90}
             >
-              {index}
+              <img src="../../img/Superman.png" alt="Superman" width="30" />
             </div>
           )}
         </Step>
         <Step>
-          {({ accomplished, transitionState, index }) => (
+          {({ accomplished, transitionState }) => (
             <div
               style={transitionStyles[transitionState]}
               className={`customStep ${accomplished ? 'accomplished' : ''}`}
               percent={100}
             >
-              {index}
+              <img src="../../img/Hulk.png" alt="Hulk" width="30" />
             </div>
           )}
         </Step>
@@ -140,44 +139,60 @@ const StepProgressBar = () => {
   );
 };
 
-/* const ProgressBar = () => {
+/* const StepProgressBar = () => {
   return (
-    <ProgressBar
-      percent={10}
-      filledBackground="linear-gradient(to right, #fefb72, #f0bb31)"
-    >
-      <Step transition="scale">
-        {({ accomplished }) => (
-          <img
-            style={{ filter: `grayscale(${accomplished ? 0 : 10}%)` }}
-            width="30"
-            src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/9d/Pichu.png/revision/latest?cb=20170407222851"
-            alt="pichu"
-          />
-        )}
-      </Step>
-      <Step transition="scale">
-        {({ accomplished }) => (
-          <img
-            style={{ filter: `grayscale(${accomplished ? 0 : 20}%)` }}
-            width="30"
-            src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/97/Pikachu_%28Smiling%29.png/revision/latest?cb=20170410234508"
-            alt="pikachu"
-          />
-        )}
-      </Step>
-      <Step transition="scale">
-        {({ accomplished }) => (
-          <img
-            style={{ filter: `grayscale(${accomplished ? 0 : 30}%)` }}
-            width="30"
-            src="https://orig00.deviantart.net/493a/f/2017/095/5/4/raichu_icon_by_pokemonshuffle_icons-db4ryym.png"
-            alt="raichu"
-          />
-        )}
-      </Step>
-    </ProgressBar>
+    <div className="progress-bar">
+      <ProgressBar
+        percent={10}
+        filledBackground="linear-gradient(to right, #fefb72, #f0bb31)"
+      >
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 10}%)` }}
+              width="30"
+              src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/9d/Pichu.png/revision/latest?cb=20170407222851"
+              alt="pichu"
+            />
+          )}
+        </Step>
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 10}%)` }}
+              width="30"
+              src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/9d/Pichu.png/revision/latest?cb=20170407222851"
+              alt="pichu"
+            />
+          )}
+        </Step>
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 20}%)` }}
+              width="30"
+              src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/97/Pikachu_%28Smiling%29.png/revision/latest?cb=20170410234508"
+              alt="pikachu"
+            />
+          )}
+        </Step>
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 30}%)` }}
+              width="30"
+              src="https://orig00.deviantart.net/493a/f/2017/095/5/4/raichu_icon_by_pokemonshuffle_icons-db4ryym.png"
+              alt="raichu"
+            />
+          )}
+        </Step>
+      </ProgressBar>
+    </div>
   );
+}; */
+
+/* StepProgressBar.propTypes = {
+  score: PropTypes.number.isRequired,
 }; */
 
 export default StepProgressBar;
