c57b3aa389a714b38ac1e0979f9737b78af8757f
Matthieu Dubo <matthieu.dubo40@gmail.com>
2021-11-18 16:34:00 +0100

    adding a page to choose between normal quiz and beat the riddler and did the css to display 2 cards to the player to choose the game he wants to play

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -5,6 +5,9 @@ import CardsQuiz from './CardsQuiz';
 import QuizResult from './QuizResult';
 import StepProgressBar from './ProgressBar';
 import BtrGame from './BtrGame';
+import './quiz.css';
+import normalQuiz from '../../img/normal-quiz-2.jpg';
+import riddlerImg from '../../img/riddler-quiz-1.jpg';
 
 const Quiz = () => {
   const [index, setIndex] = useState(0);
@@ -19,25 +22,36 @@ const Quiz = () => {
     <div className="container-quiz">
       <Navbar />
       {chooseGame && (
+        <div className="container_choose-quiz">
+          <div className="choose-quiz">
+            <h2>Choose le jeu que tu veux</h2>
+          </div>
+        </div>
+      )}
+      {chooseGame && (
         <div className="container-btn-game">
-          <button
-            type="button"
+          <div
+            role="presentation"
+            className="choose-game"
             onClick={() => {
               setChooseGame(false);
               setNormalGame(true);
             }}
           >
-            Normal Game
-          </button>
-          <button
-            type="button"
+            <img src={normalQuiz} alt="Superhero" />
+            <h2>Normal Quiz</h2>
+          </div>
+          <div
+            role="presentation"
+            className="choose-game"
             onClick={() => {
               setChooseGame(false);
               setBtr(true);
             }}
           >
-            Beat The Riddler
-          </button>
+            <img src={riddlerImg} alt="Riddler" />
+            <h2>Beat The Riddler</h2>
+          </div>
         </div>
       )}
       {normalGame && <StepProgressBar score={score} />}
